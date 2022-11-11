a9378ba1b199bf121ddad25767bc38f0363d8388
lenbo <allenbora@gmail.com>
2022-11-11 10:40:56 +0800

    Eleventh

---
+++ AbsFunction.py
@@ -0,0 +1,15 @@
+#========abs() demonstration==========
+#set A example for abs function.......
+import math
+x0 = 2
+x1 = -7
+
+#print(abs(x0))
+#print(abs(x1))
+#===============================================================
+#Set B example for abs function......
+
+x_list = [1, -2, -3]
+
+for item in x_list:
+    print(abs(item))
